b728eeb6919c7df89ae307a4adf45e57fff02c22
komara1964 <komara.alexandr@gmail.com>
2016-12-29 23:26:47 +0400

    Commit 3

--- source/conf.py
+++ source/conf.py
@@ -86,15 +86,17 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'alabaster'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 #[sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+'body_text_align':'justify',
+'font_family':'tahoma, sans'}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
