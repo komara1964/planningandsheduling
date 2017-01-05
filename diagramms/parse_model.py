86051fe61ec63c82f990cbcb40ae8d76974e46e4
komara1964 <komara.alexandr@gmail.com>
2017-01-05 23:37:33 +0400

    commit5

--- diagramms/parse_model.py
+++ diagramms/parse_model.py
@@ -2,16 +2,15 @@
 
 import lxml.etree as et
 
-md = et.parse("/home/alexandr/Документы/planningandsheduling/diagramms/initial_model/domain_object_model.uxf")
+md = et.parse(r"E:\plan_shedule\planningandsheduling\diagramms\initial_model\domain_object_model.uxf")
 
-out_f = open("/home/alexandr/Документы/planningandsheduling/diagramms/initial_model/parsing_res.txt", "w", encoding="utf-8")
+out_f = open(r"E:\plan_shedule\planningandsheduling\diagramms\initial_model\parsing_res.txt", "w", encoding="utf-8")
 
 for x in md.iter("element"):
     ids = x.find("id")
     if ids is not None and ids.text == "UMLClass":
         pnl_attr = x.find("panel_attributes")
         if pnl_attr is not None:
-            s = pnl_attr.text
             d = pnl_attr.text.splitlines()
             print(d)
             out_f.write(".. py:class:: " + d[0] + '\n')
---
+++ p27.bat
@@ -0,0 +1,7 @@
+cd e:
+
+cd E:\flaskenv\vtnv27
+
+call scripts\activate
+
+cd E:\plan_shedule\planningandsheduling
