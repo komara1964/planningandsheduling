65863cabb793380df1bcd687b275871890174bb0
komara1964 <komara.alexandr@gmail.com>
2017-01-27 23:55:23 +0400

    commit_27_1

---
+++ diagramms/prepare_csv.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+input_file = open("C:/Users/Public/Documents/p30.csv", "rt", encoding="cp1251")
+first_l = input_file.readline()
+d = first_l.split(";")
+print(len(d))
+print(first_l)
+m_f = {}
+for line in input_file.readlines():
+    print(line)
+    d = line.rstrip().split(";")
+    if d[1] in m_f:
+        if d[0] in m_f[d[1]]:
+            m_f[d[1]][d[0]].extend(d[2:])
+        else:
+            m_f[d[1]][d[0]] = d[2:]
+    else:
+        m_f[d[1]] = {}
+        m_f[d[1]][d[0]] = []
+        m_f[d[1]][d[0]].extend(d[2:])
+print("**************************************")
+
+outf = open("C:/Users/Public/Documents/p30.txt", "w", encoding="utf-8")
+for key, value in m_f.items():
+    print(key)
+    outf.write(key)
+    outf.write("\n")
+    outf.write("\n")
+    for lk, lv in value.items():
+        outf.write(lk + "\n")
+        outf.write("-".ljust(len(lk), "-"))
+        outf.write("\n")
+        offset = 0
+        outf.write("\n")
+        while offset < len(lv) - 1:
+            outf.write(" ".ljust(3, " "))
+            outf.write('"' + lv[offset] + '"')
+            outf.write("," + '"' +  lv[offset + 1] + '"')
+            outf.write("," + '"' +  lv[offset + 2] + '"')
+            outf.write("," + '"' +  lv[offset + 3] + '"')
+            outf.write("\n")
+            offset = offset + 4
+        outf.write("\n")    
+input_file.close()
+outf.close()
+print("Все OK")
